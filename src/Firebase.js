d3dd6aa03c07393b71a9f54c165842684d7193ce
kevinmun <kevin_mkt@hotmail.com>
2017-12-18 11:05:22 +0800

    removed firebase config details

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,12 +1,9 @@
 import * as firebase from 'firebase';
 
+/**
+ * Insert firebase config details
+ */
 const config = {
-  apiKey: 'AIzaSyD7hDTuqCuaYf3x-DjsqqZLA3V9wopYWrU',
-  authDomain: 'plan-b09fd.firebaseapp.com',
-  databaseURL: 'https://plan-b09fd.firebaseio.com',
-  projectId: 'plan-b09fd',
-  storageBucket: 'plan-b09fd.appspot.com',
-  messagingSenderId: '1058859357432',
 };
 firebase.initializeApp(config);
 
